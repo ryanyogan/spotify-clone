5f31cdace5aab01cc7389ae8a042cf40c1806473
Ryan Yogan <ryanyogan@gmail.com>
2023-07-13 10:52:47 -0500

    liked button

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,9 +1,21 @@
+import Header from "@/components/header";
+import ListItem from "@/components/list-item";
+
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h1>main</h1>
-      </div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem
+              image="/images/liked.png"
+              href="/liked"
+              name="Liked Songs"
+            />
+          </div>
+        </div>
+      </Header>
     </div>
   );
 }
---
+++ src/components/button.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import clsx from "clsx";
+import { forwardRef } from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        type={type}
+        className={clsx(
+          "w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 font-bold hover:opacity-75 transition",
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
